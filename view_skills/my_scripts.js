c3334d0ed821395e7231b5a5a102268e6023ea4a
Christopher Schechter <chrisschek@users.noreply.github.com>
2021-09-10 12:51:25 -1000

    redid master skill list to completely remove JSON plus supporting refactors

--- view_skills/my_scripts.js
+++ view_skills/my_scripts.js
@@ -4,100 +4,123 @@ const TAG_TANGO_ENERGY = '<img src="static/Tango-energy.png" class="tango"/>';
 const TAG_TANGO_ACTIVATION = '<img src="static/Tango-activation-darker.png" class="tango"/>';
 const TAG_TANGO_RECHARGE = '<img src="static/Tango-recharge-darker.png" class="tango"/>';
 
-const PROF_TO_CSS_CLASS = {
-    'Warrior': 'row-war',
-    'Ranger': 'row-ran',
-    'Monk': 'row-mon',
-    'Necromancer': 'row-nec',
-    'Mesmer': 'row-mes',
-    'Elementalist': 'row-ele',
-    'Assassin': 'row-ass',
-    'Ritualist': 'row-rit',
-    'Paragon': 'row-par',
-    'Dervish': 'row-der',
-    'Common': 'row-com',
-    null: 'row-com',
+const PROF = {
+    'war': { 'name': 'Warrior', 'class': 'row-war', },
+    'ran': { 'name': 'Ranger', 'class': 'row-ran', },
+    'mon': { 'name': 'Monk', 'class': 'row-mon', },
+    'nec': { 'name': 'Necromancer', 'class': 'row-nec', },
+    'mes': { 'name': 'Mesmer', 'class': 'row-mes', },
+    'ele': { 'name': 'Elementalist', 'class': 'row-ele', },
+    'ass': { 'name': 'Assassin', 'class': 'row-ass', },
+    'rit': { 'name': 'Ritualist', 'class': 'row-rit', },
+    'par': { 'name': 'Paragon', 'class': 'row-par', },
+    'der': { 'name': 'Dervish', 'class': 'row-der', },
+    'com': { 'name': 'Common', 'class': 'row-com', },
+    null: { 'name': 'Common', 'class': 'row-com', },
 };
+
+///////////////////////
+/////////////////////// Randomization
+///////////////////////
+
+var filters = {
+    'name': [],
+    'camp': [],
+    'prof': [],
+    'attr': [],
+}
+
+var autoinclude = [
+    'Resurrection Signet',
+    'Comfort Animal',
+]
+
+function generateRandomSkillPool(poolSize, filters, autoinclude) {
+
+}
+
 
 ///////////////////////
 /////////////////////// Table creation
 ///////////////////////
 
-function constructTable(tableSelector, list) {
+function constructTable(tableSelector, skillList) {
 
     var table = $(tableSelector);
 
     var tbody = $('<tbody/>');
     table.append(tbody);
 
-    for (var i = 0; i < list.length; i++) {
-        var skillObj = list[i];
+    for (var i = 0; i < skillList.length; i++) {
+        var skill = skillList[i];
+        // console.log('Adding skill: ' + skill.name);
 
         var row = $('<tr/>');
-        row.addClass(PROF_TO_CSS_CLASS[skillObj.profession])
+        row.addClass(PROF[skill.prof]['class']);
 
-        row.append(createIconCell(skillObj));
-        row.append(createNameCell(skillObj));
-        row.append(createDescriptionCell(skillObj));
-        row.append(createCostsCell(skillObj));
-        row.append(createAttributeCell(skillObj));
-        row.append(createCampaignCell(skillObj));
+        row.append(createIconCell(skill));
+        row.append(createNameCell(skill));
+        row.append(createDescriptionCell(skill));
+        row.append(createCostsCell(skill));
+        row.append(createAttributeCell(skill));
+        row.append(createCampaignCell(skill));
 
         tbody.append(row);
     }
 }
 
-function createIconCell(skillObj) {
-    var imgTag = $('<img/>').attr('src', skillObj.image_url).addClass('center-icon');
+function createIconCell(skill) {
+    var iconUrl = getIconUrl(skill);
+    var imgTag = $('<img/>').attr('src', iconUrl).addClass('center-icon');
     return $('<td/>').html(imgTag);
 }
 
-function createNameCell(skillObj) {
-    var name = skillObj.name;
-    var href = skillObj.url;
+function createNameCell(skill) {
+    var name = skill.name;
+    var href = getWikiUrl(skill);
     var link = $('<a/>').html(name).attr('href', href).addClass('skill-name');
     return $('<td/>').html(link).addClass('skills-table-cell');
 }
 
-function createDescriptionCell(skillObj) {
-    var desc = skillObj.description;
+function createDescriptionCell(skill) {
+    var desc = skill.desc;
     return $('<td/>').html(desc).addClass('skills-table-cell').attr('style', 'text-align:left;');
 }
 
-function createCostsCell(skillObj) {
+function createCostsCell(skill) {
     var costs = $('<div/>');
 
-    if (skillObj['sacrifice'] != null) {
-        var span = $('<span/>').addClass('no-wrap').html(skillObj['sacrifice'] + TAG_TANGO_SACRIFICE);
+    if (skill.sac != null) {
+        var span = $('<span/>').addClass('no-wrap').html(skill.sac + TAG_TANGO_SACRIFICE);
         costs.append(span);
     }
-    if (skillObj['adrenaline'] != null) {
-        var span = $('<span/>').addClass('no-wrap').html('  ' + skillObj['adrenaline'] + ' ' + TAG_TANGO_ADRENALINE);
+    if (skill.adr != null) {
+        var span = $('<span/>').addClass('no-wrap').html('  ' + skill.adr + ' ' + TAG_TANGO_ADRENALINE);
         costs.append(span);
     }
-    if (skillObj['energy'] != null) {
-        var span = $('<span/>').addClass('no-wrap').html('  ' + skillObj['energy'] + ' ' + TAG_TANGO_ENERGY);
+    if (skill.ene != null) {
+        var span = $('<span/>').addClass('no-wrap').html('  ' + skill.ene + ' ' + TAG_TANGO_ENERGY);
         costs.append(span);
     }
-    if (skillObj['activation_time'] != null) {
-        var span = $('<span/>').addClass('no-wrap').html('  ' + skillObj['activation_time'] + ' ' + TAG_TANGO_ACTIVATION);
+    if (skill.act != null) {
+        var span = $('<span/>').addClass('no-wrap').html('  ' + skill.act + ' ' + TAG_TANGO_ACTIVATION);
         costs.append(span);
     }
-    if (skillObj['recharge_time'] != null) {
-        var span = $('<span/>').addClass('no-wrap').html('  ' + skillObj['recharge_time'] + ' ' + TAG_TANGO_RECHARGE);
+    if (skill.rec != null) {
+        var span = $('<span/>').addClass('no-wrap').html('  ' + skill.rec + ' ' + TAG_TANGO_RECHARGE);
         costs.append(span);
     }
 
     return $('<td/>').html(costs).addClass('skills-table-cell');
 }
 
-function createAttributeCell(skillObj) {
-    var attribute = skillObj.attribute;
+function createAttributeCell(skill) {
+    var attribute = skill.attr;
     return $('<td/>').html(attribute).addClass('skills-table-cell');
 }
 
-function createCampaignCell(skillObj) {
-    var campaign = skillObj.campaign;
+function createCampaignCell(skill) {
+    var campaign = skill.camp;
     return $('<td/>').html(campaign).addClass('skills-table-cell');
 }
 
@@ -108,15 +131,15 @@ function createCampaignCell(skillObj) {
 function enableFilterButtons() {
     $('#filter-all').click(function(){
         console.log('Doing filter-all');
-        for (var key in PROF_TO_CSS_CLASS) {
-            var className = PROF_TO_CSS_CLASS[key];
+        for (var key in PROF) {
+            var className = PROF[key]['class'];
             $('.' + className).show();
         }
     });
 
     let hideAllFilterTypes = function() {
-        for (var key in PROF_TO_CSS_CLASS) {
-            var className = PROF_TO_CSS_CLASS[key];
+        for (var key in PROF) {
+            var className = PROF[key]['class'];
             $('.' + className).hide();
         }
     }
@@ -141,16 +164,18 @@ var skillsTable;
 $(document).ready(function(){
 
     console.log('Starting JSON parse & skills-table creation');
-    var allSkillsList = JSON.parse(allSkillsJson);
-    constructTable('#skills-table', allSkillsList);
+    var skillsList = SKILL_LIST_MASTER;
+    constructTable('#skills-table', skillsList);
     console.log('Finished creating table');
 
     console.log('Starting DataTable creation');
     skillsTable = $('#skills-table').DataTable({
         paging: false,
         columnDefs: [
-            // Disables features on first column ('Icon' column)
-            { targets: 0, orderable: false, searchable: false }
+            // Disables features on zeroth column ('Icon' column)
+            { targets: 0, orderable: false, searchable: false },
+            // Disables features on third column ('Costs' column)
+            { targets: 3, orderable: false, searchable: false },
           ]
     });
     console.log('Finished DataTable creation');
